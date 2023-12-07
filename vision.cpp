020e5305549cd23e4b677873be9d21016e3ee458
jhoon6 <wjdgns6615@naver.com>
2023-12-07 09:59:01 +0900

    update

--- vision.cpp
+++ vision.cpp
@@ -28,51 +28,52 @@ int calc_err(Mat gray_img) {
     cvtColor(gray_img, dst, COLOR_GRAY2BGR);
     static Point center_variable_l(160, 45);
     static Point center_variable_r(160+320, 45);
-    Rect fit_l;
-    Rect fit_r;
 
-    static int error = 0;
-    int sel_cnt=0;
+    int min_norm_l = INT_MAX;
+    int min_norm_r = INT_MAX;
+
+    Rect sel_l;
+    Rect sel_r;
 
     for (int i = 1; i < cnt; i++) {
         int* p = stats.ptr<int>(i);
         Rect r(p[0], p[1], p[2], p[3]);
-        rectangle(dst, r, Scalar(150,150,150),1,8);
-        
-        //putText(dst, format("%d", i), Point(p[0] + 25, p[1] + 25), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 0), 2);
+        rectangle(dst, r, Scalar(150, 150, 150), 1, 8);
 
         Point r_center = (r.br() + r.tl()) * 0.5;
+        int diff_length = norm(center_variable_l - r_center);
         drawMarker(dst, r_center, Scalar(0, 0, 255), 0, 10, 2, 8);
         //L
-        if (p[1]+p[3] > 80) {
-            int diff = (center_variable_l.x - r_center.x);
-            if (diff < 180 && diff > -180 && norm(center_variable_l-r_center) < 180) {
-                //drawMarker(dst, findCentroid(cut_gray, r), Scalar(255, 128, 0), 1, 10, 2, 8);
-                center_variable_l = r_center;
-                fit_l = r;
-                sel_cnt++;
-            }
+        if (min_norm_l > diff_length && p[1] + p[3] > 60 && diff_length < 120) {
+            sel_l = r;
+            min_norm_l = diff_length;
+            center_variable_l = r_center;
         }
-        //R
-        if (p[1]+p[3] > 80) {
-            int diff = (center_variable_r.x - r_center.x);
-            if (diff < 180 && diff > -180 && norm(center_variable_r-r_center) < 180) {
-                //drawMarker(dst, findCentroid(cut_gray, r), Scalar(255, 128, 0), 1, 10, 2, 8);
-                center_variable_r = r_center;
-                fit_r = r;
-                sel_cnt++;
-            }
-        }
-
     }
-    if(!fit_l.empty()) rectangle(dst, fit_l, Scalar(0, 255, 255), 2);
-    if(!fit_r.empty()) rectangle(dst, fit_r, Scalar(0, 255, 255), 2);
+
+    for (int i = 1; i < cnt; i++) {
+        int* p = stats.ptr<int>(i);
+        Rect r(p[0], p[1], p[2], p[3]);
+
+        Point r_center = (r.br() + r.tl()) * 0.5;
+        int diff_length = norm(center_variable_r - r_center);
+        drawMarker(dst, r_center, Scalar(0, 0, 255), 0, 10, 2, 8);
+        //R
+        if (min_norm_r > diff_length && p[1] + p[3] > 60 && diff_length < 120) {
+            sel_r = r;
+            min_norm_r = diff_length;
+            center_variable_r = r_center;
+        }
+    }
+
+    if (!sel_l.empty()) rectangle(dst, sel_l, Scalar(0, 255, 255), 2);
+    if (!sel_r.empty()) rectangle(dst, sel_r, Scalar(0, 255, 255), 2);
     drawMarker(dst, center_variable_l, Scalar(255, 128, 255), 3, 10, 2, 8);
     drawMarker(dst, center_variable_r, Scalar(255, 128, 255), 3, 10, 2, 8);
-    line(dst, center_variable_l,center_variable_r, Scalar(255,0,128),2);
+    line(dst, center_variable_l, center_variable_r, Scalar(255, 0, 128), 2);
 
     Point pt_err((center_variable_l.x+center_variable_r.x)/2, (center_variable_l.y+center_variable_r.y)/2);
     drawMarker(dst, pt_err, Scalar(200, 0, 0), 4, 10, 2, 8);
     writer2 << dst;
-    return error = pt_err.x;
+    return pt_err.x;
 }
